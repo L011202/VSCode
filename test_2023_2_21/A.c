d0b88669405dab3f97181bdcfa7f84af23f3a4f8
L011202 <lpeng1202@gmail.com>
2024-03-29 10:35:12 +0800

    代码提交

---
+++ test_2023_2_21/A.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+int sumlate(int s,int n)
+{
+	int a;
+	int b;
+	int x;
+	x = a * b;
+	return x;
+}
+
+int main()
+{
+	int a;
+	int b;
+	int x;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	printf("a=%d", a\n);
+	printf("b=%d", b\n);
+	printf("x=d", x\n);
+	return 0;
+}
