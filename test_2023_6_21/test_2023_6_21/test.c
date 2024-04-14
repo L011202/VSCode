4365b32b80079522e0d3a47044035d72449e0b66
L011202 <lpeng1202@gmail.com>
2024-04-14 23:51:50 +0800

    C语言学习---代码提交
    Signed-off-by: L011202 <lpeng1202@gmail.com>

---
+++ test_2023_6_21/test_2023_6_21/test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+/*
+//以下为测试代码，测试版本更新后的运行是否有无问题以及调试(程序过于简单，调试暂未得知)
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = a + b;
+//	printf("%d\n", c);
+//	return 0;
+//}
+*/
+/*
+//**@C语言学习P29
+
+*/
+int main()
+{
+	char a = 128;
+	printf("%d\n", a);
+	//%d打印十进制的有符号数
+	//%u打印十进制的无符号数
+	return 0;
+}
